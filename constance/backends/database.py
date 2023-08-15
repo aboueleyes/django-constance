9692ada109b6d2c2f644867561fb8e3d015c1f4f
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2023-08-15 22:02:03 +0300

    Support Caching in mget.

--- constance/backends/database.py
+++ constance/backends/database.py
@@ -1,3 +1,4 @@
+import contextlib
 from django.core.cache import caches
 from django.core.cache.backends.locmem import LocMemCache
 from django.core.exceptions import ImproperlyConfigured
@@ -16,15 +17,17 @@ from constance import settings, signals, config
 class DatabaseBackend(Backend):
     def __init__(self):
         from constance.models import Constance
+
         self._model = Constance
         self._prefix = settings.DATABASE_PREFIX
         self._autofill_timeout = settings.DATABASE_CACHE_AUTOFILL_TIMEOUT
-        self._autofill_cachekey = 'autofilled'
+        self._autofill_cachekey = "autofilled"
 
         if self._model._meta.app_config is None:
             raise ImproperlyConfigured(
                 "The constance.backends.database app isn't installed "
-                "correctly. Make sure it's in your INSTALLED_APPS setting.")
+                "correctly. Make sure it's in your INSTALLED_APPS setting."
+            )
 
         if settings.DATABASE_CACHE_BACKEND:
             self._cache = caches[settings.DATABASE_CACHE_BACKEND]
@@ -33,7 +36,8 @@ class DatabaseBackend(Backend):
                     "The CONSTANCE_DATABASE_CACHE_BACKEND setting refers to a "
                     "subclass of Django's local-memory backend (%r). Please "
                     "set it to a backend that supports cross-process caching."
-                    % settings.DATABASE_CACHE_BACKEND)
+                    % settings.DATABASE_CACHE_BACKEND
+                )
         else:
             self._cache = None
         self.autofill()
@@ -41,7 +45,7 @@ class DatabaseBackend(Backend):
         post_save.connect(self.clear, sender=self._model)
 
     def add_prefix(self, key):
-        return "%s%s" % (self._prefix, key)
+        return f"{self._prefix}{key}"
 
     def autofill(self):
         if not self._autofill_timeout or not self._cache:
@@ -49,8 +53,7 @@ class DatabaseBackend(Backend):
         full_cachekey = self.add_prefix(self._autofill_cachekey)
         if self._cache.get(full_cachekey):
             return
-        autofill_values = {}
-        autofill_values[full_cachekey] = 1
+        autofill_values = {full_cachekey: 1}
         for key, value in self.mget(settings.CONFIG):
             autofill_values[self.add_prefix(key)] = value
         self._cache.set_many(autofill_values, timeout=self._autofill_timeout)
@@ -59,12 +62,17 @@ class DatabaseBackend(Backend):
         if not keys:
             return
         keys = {self.add_prefix(key): key for key in keys}
-        try:
+        if self._cache:
+            values = self._cache.get_many(keys)
+            for key, value in values.items():
+                yield keys[key], value
+            keys = set(keys) - set(values)
+            if not keys:
+                return
+        with contextlib.suppress(OperationalError, ProgrammingError):
             stored = self._model._default_manager.filter(key__in=keys)
             for const in stored:
                 yield keys[const.key], const.value
-        except (OperationalError, ProgrammingError):
-            pass
 
     def get(self, key):
         key = self.add_prefix(key)
